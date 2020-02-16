37f2036aa2901a6f3f87609df63e23a4f3015fb7
vermaajit05 <vermaajit05@gmail.com>
2020-02-16 17:54:02 +0530

    Add files via upload

---
+++ resumeparserapi.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 22:24:55 2020
+
+@author: verma
+"""
+
+
+
+import os
+import spacy
+from spacy.matcher import Matcher
+import constants as cs
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords 
+import re
+import pandas as pd
+from flask import Flask, request
+from flasgger import Swagger
+from flask import jsonify
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/predict_file', methods=["POST"])
+def predict_iris_file():
+    data = (request.files.get("fieldNameHere")).read().decode()
+    entity = extract_entity_sections(data)
+    return jsonify(entity)
+if __name__ == '__main__':
+    #nlp = spacy.load('en_core_web_sm')
+    app.run()
+    
+def extract_mobile_number(text):
+    '''
+    Helper function to extract mobile number from text
+
+    :param text: plain text extracted from resume file
+    :return: string of extracted mobile numbers
+    '''
+    # Found this complicated regex on : https://zapier.com/blog/extract-links-email-phone-regex/
+    phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
+def extract_email(text):
+    '''
+    Helper function to extract email id from text
+
+    :param text: plain text extracted from resume file
+    '''
+    email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", text)
+    if email:
+        try:
+            return email[0].split()[0].strip(';')
+        except IndexError:
+            return None
+
+     
+def extract_skills(nlp_text, noun_chunks):
+    '''
+    Helper function to extract skills from spacy nlp text
+
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :param noun_chunks: noun chunks extracted from nlp text
+    :return: list of skills extracted
+    '''
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+    data = pd.read_csv('skills.csv')
+    skills = list(data.columns.values)
+    skillset = []
+    # check for one-grams
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+    
+    # check for bi-grams and tri-grams
+    for token in noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+def extract_name(nlp_text, matcher):
+    '''
+    Helper function to extract name from spacy nlp text
+
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :param matcher: object of `spacy.matcher.Matcher`
+    :return: string of full name
+    '''
+    pattern = [cs.NAME_PATTERN]
+    
+    matcher.add('NAME', None, *pattern)
+    
+    matches = matcher(nlp_text)
+    
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        return span.text
+
+def extract_experience(resume_text):
+    '''
+    Helper function to extract experience from resume text
+
+    :param resume_text: Plain resume text
+    :return: list of experience
+    '''
+    wordnet_lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    # word tokenization 
+    word_tokens = nltk.word_tokenize(resume_text)
+
+    # remove stop words and lemmatize  
+    filtered_sentence = [w for w in word_tokens if not w in stop_words and wordnet_lemmatizer.lemmatize(w) not in stop_words] 
+    sent = nltk.pos_tag(filtered_sentence)
+
+    # parse regex
+    cp = nltk.RegexpParser('P: {<NNP>+}')
+    cs = cp.parse(sent)
+    
+    # for i in cs.subtrees(filter=lambda x: x.label() == 'P'):
+    #     print(i)
+    
+    test = []
+    
+    for vp in list(cs.subtrees(filter=lambda x: x.label()=='P')):
+        test.append(" ".join([i[0] for i in vp.leaves() if len(vp.leaves()) >= 2]))
+
+    # Search the word 'experience' in the chunk and then print out the text after it
+    x = [x[x.lower().index('experience') + 10:] for i, x in enumerate(test) if x and 'experience' in x.lower()]
+    return x
+
+def extract_education(nlp_text):
+    '''
+    Helper function to extract education from spacy nlp text
+
+    :param nlp_text: object of `spacy.tokens.doc.Doc`
+    :return: tuple of education degree and year if year if found else only returns education degree
+    '''
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in cs.EDUCATION and tex not in cs.STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(cs.YEAR), edu[key])
+        if year:
+            education.append((key, ''.join(year.group(0))))
+        else:
+            education.append(key)
+    return education
+
+
+def extract_entity_sections(text):
+    '''
+    Helper function to extract all the raw text from sections of resume
+
+    :param text: Raw text of resume
+    :return: dictionary of entities
+    '''
+    text_split = [i.strip() for i in text.split('\n')]
+    # sections_in_resume = [i for i in text_split if i.lower() in sections]
+    entities = {}
+    key = False
+    for phrase in text_split:
+        if len(phrase) == 1:
+            p_key = phrase
+        else:
+            p_key = set(phrase.lower().split()) & set(cs.RESUME_SECTIONS)
+        try:
+            p_key = list(p_key)[0]
+        except IndexError:
+            pass
+        if p_key in cs.RESUME_SECTIONS:
+            entities[p_key] = []
+            key = p_key
+        elif key and phrase.strip():
+            entities[key].append(phrase)
+    
+    # entity_key = False
+    # for entity in entities.keys():
+    #     sub_entities = {}
+    #     for entry in entities[entity]:
+    #         if u'\u2022' not in entry:
+    #             sub_entities[entry] = []
+    #             entity_key = entry
+    #         elif entity_key:
+    #             sub_entities[entity_key].append(entry)
+    #     entities[entity] = sub_entities
+
+    # pprint.pprint(entities)
+
+    # make entities that are not found None
+    # for entity in cs.RESUME_SECTIONS:
+    #     if entity not in entities.keys():
+    #         entities[entity] = None 
+    return entities
+    
